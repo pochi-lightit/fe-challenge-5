db8bf2b9d33afa3a6bbdb125305495e319e7c9c2
Flor Pocetti <pochi@192.168.1.3>
2024-01-08 12:16:47 -0300

    adding styles with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      //me traigo las imágenes que voy a usar en el proyecto
+      backgroundImage: {
+        logo_blue: "url('../img/logo-blue.svg')",
+        microscope: "url('../img/microscope.jpg')",
+        needle: "url('../img/needle.jpg')",
+        spa: "url('../img/spa.jpg')",
+        trafalgar_footer: "url('../img/trafalgar-footer.png')",
+        trafalgar_header: "url('../img/trafalgar-header.png')",
+        dots_3x5: "url('../img/dots-3x5.png')",
+        dots_5x5: "url('../img/dots-5x5.svg')",
+      },
+      //defino los colores que voy a usar en el proyecto, importando los disponibles en tailwind (...theme('colors')) y agregando otros:
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#458FF6",
+      }),
+      textColor: {
+        primary: "#458FF6",
+        secondary: "#7D7987",
+        tertiary: "#FFFFFF",
+        highlight: "#4089ED",
+        menu: "#1F1534",
+      },
+      fontFamily: {
+        //se agregan como arreglos []
+        ubuntu: ["Ubuntu", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
